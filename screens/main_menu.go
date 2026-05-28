32dc17dec5fd975826c8ec5452d08d92f3aa4f05
El Lampros <h4.zer21@gmail.com>
2026-05-28 18:19:13 +0700

    Update

--- screens/main_menu.go
+++ screens/main_menu.go
@@ -69,7 +69,7 @@ func RenderMainMenu(cursor, width, height int) string {
 	)
 	versionLine := lipgloss.NewStyle().
 		Foreground(lipgloss.Color(ui.ColComment)).
-		Render("  \uf489  lazypoot v2.1.0  ·  proot-distro manager")
+		Render("  \uf489  lazypoot v2.2.0  ·  proot-distro manager")
 
 	panel := ui.StyleBoxBlue.
 		Padding(1, 4).
